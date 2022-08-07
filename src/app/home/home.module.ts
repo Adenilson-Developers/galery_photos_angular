7f9267d026e2f7dd9315472210e08532ea245213
Adenilson-Developers <adenilson.developers@gmail.com>
2022-08-06 22:36:32 -0300

    adicionando validação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { SignInComponent } from "./signin/signin.component";
 
 @NgModule({
     declarations: [SignInComponent],
 
-    imports: [],
+    imports: [ ReactiveFormsModule, CommonModule ],
 
-    exports: []
+    exports: [  ]
 })
 
 export class HomeModule {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+
+    loginForm: FormGroup;
+
+    constructor( private formBilder: FormBuilder){}
+
+    ngOnInit(): void {
+        this.loginForm = this.formBilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
+}
