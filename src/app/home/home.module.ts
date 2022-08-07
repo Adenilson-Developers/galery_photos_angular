e68427a7b8274233041279ff76651d38af409431
Adenilson-Developers <adenilson.developers@gmail.com>
2022-08-06 23:10:35 -0300

    componetizando menssage de errrors

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from "./signin/signin.component";
 
 @NgModule({
     declarations: [SignInComponent],
 
-    imports: [ ReactiveFormsModule, CommonModule ],
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule,
+        VmessageModule
+       
+         ],
 
     exports: [  ]
 })
---
+++ src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,9 @@
+import { VmessageComponent } from './vmessage.component';
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [ VmessageComponent ],
+    exports: [ VmessageComponent ]
+})
+
+export class VmessageModule {}
