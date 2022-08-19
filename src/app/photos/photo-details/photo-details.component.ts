192b87c7f1599066d66f9cac247516afec891b40
Adenilson-Developers <adenilson.developers@gmail.com>
2022-08-18 21:27:30 -0300

    adicionando css

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from "@angular/core";
 import { Photo } from '../photo/photo';
 
 @Component({
-    templateUrl: './photo-details.component.html'
+    templateUrl: './photo-details.component.html',
+    styleUrls: ['./photo-details.css']
 })
 
 export class PhotoDetailsComponent implements OnInit {
