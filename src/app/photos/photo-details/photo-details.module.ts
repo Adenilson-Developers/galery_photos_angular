a5b2d6d5063c3ea10b69fdbb438a39d6d200bf9b
Adenilson-Developers <adenilson.developers@gmail.com>
2022-08-31 16:25:39 -0300

    exibindo likes so para usuarios logados

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { PhotoModule } from './../photo/photo.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 import { PhotoOwnerOnlyDirective } from './photo-woner-only/photo-owner-only.directive';
+import { ShowIfLogedModule } from 'src/app/shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
     declarations: [
@@ -26,7 +28,8 @@ import { PhotoOwnerOnlyDirective } from './photo-woner-only/photo-owner-only.dir
         PhotoModule,
         RouterModule,
         ReactiveFormsModule,
-        VmessageModule
+        VmessageModule,
+        ShowIfLogedModule
     ]
 })
 
